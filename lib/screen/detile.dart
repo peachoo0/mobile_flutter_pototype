f279758582db50b3ffe67b43e8d6af7013043196
peachoo0 <71977600+peachoo0@users.noreply.github.com>
2022-02-28 21:54:58 +0700

    update web 28022565

---
+++ lib/screen/detile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Detilie extends StatefulWidget {
+  Detilie({Key key}) : super(key: key);
+
+  @override
+  State<Detilie> createState() => _DetilieState();
+}
+
+class _DetilieState extends State<Detilie> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
